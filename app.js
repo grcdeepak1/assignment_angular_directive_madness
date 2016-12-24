841c904a5580872b3a0a2ad832269c48c28a371a
Deepak <grc.deepak@gmail.com>
2016-12-23 17:03:42 -0800

    nav directive, transclusion, jqlite

--- app.js
+++ app.js
@@ -3,3 +3,52 @@ directiveMadness.controller('MainCtrl', ['$scope', function($scope) {
 
   }]
 );
+directiveMadness.directive('mainNav', function() {
+  return {
+    templateUrl: 'directives/mainNav.html',
+    restrict: "E",
+    scope: {}
+  };
+})
+directiveMadness.directive('radioForm', function() {
+  return {
+    templateUrl: 'directives/radioForm.html',
+    restrict: "E",
+    scope: {}
+  };
+})
+directiveMadness.directive('mainHeader', function() {
+  return {
+    template: '<h1><ng-transclude></ng-transclude></h1>',
+    restrict: "E",
+    transclude: true,
+    scope: {}
+  };
+})
+directiveMadness.directive('copyright', function() {
+  function copyrightCallback(scope, element, attributes){
+    var dteNow = new Date();
+    var intYear = dteNow.getFullYear();
+    element.find('span').html(element.find('span').html() + ' '+ intYear);
+  }
+  return {
+    templateUrl: 'directives/copyright.html',
+    restrict: "E",
+    transclude: true,
+    scope: {},
+    link: copyrightCallback
+  };
+})
+directiveMadness.directive('colorize', function() {
+  function colorizeCallback(scope, element, attributes){
+    element.css("background-color", scope.background).css("color", scope.color);
+  }
+  return {
+    restrict: "A",
+    scope: {
+      color: '@',
+      background: '@'
+    },
+    link: colorizeCallback
+  };
+})
