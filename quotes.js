c035e260591fda444ff3d69b0d5783088245c49f
Deepak <grc.deepak@gmail.com>
2016-12-24 10:58:50 -0800

    fix form sub

---
+++ quotes.js
@@ -0,0 +1,34 @@
+directiveMadness.controller('QuotesCtrl', ['$scope', function($scope) {
+  $scope.quotes = [ { message: "Don't cry because it's over, smile because it happened.", author: "Dr. Seuss" },
+                    { message: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe", author: " Albert Einstein" },
+                  ];
+  $scope.name = "Deepak"
+  $scope.quoteParams = { }
+  $scope.createQuote = function() {
+    var quote = {
+       message: $scope.quoteParams.quotemessage,
+       author: $scope.quoteParams.quoteauthor
+     };
+     $scope.quotes.push(quote);
+     $scope.quoteParams.quotemessage = $scope.quoteParams.quoteauthor = "";
+  };
+  $scope.deleteQuote = function(quote) {
+    var index = $scope.quotes.indexOf(quote);
+    $scope.quotes.splice(index, 1)
+  }
+  }]
+);
+directiveMadness.directive('quoteForm', function() {
+  return {
+    templateUrl: 'directives/quoteForm.html',
+    restrict: "E",
+    scope: true
+  };
+})
+directiveMadness.directive('quotesIndex', function() {
+  return {
+    templateUrl: 'directives/quotesIndex.html',
+    restrict: "E",
+    scope: true
+  };
+})
