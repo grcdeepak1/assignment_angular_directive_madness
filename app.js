eaf2c2d39d2dbde79019bee9b9e3f764dccc6273
Deepak <grc.deepak@gmail.com>
2016-12-23 20:24:57 -0800

    Quotes creating and listing

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 var directiveMadness = angular.module('directiveMadness', []);
-directiveMadness.controller('MainCtrl', ['$scope', function($scope) {
+
+directiveMadness.controller('QuotesCtrl', ['$scope', function($scope) {
+  $scope.quotes = [ { message: "Don't cry because it's over, smile because it happened.", author: "Dr. Seuss" },
+                    { message: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe", author: " Albert Einstein" },
+                  ];
+  $scope.quoteMessage = "Hello World";
+  $scope.quoteAuthor = "Deepak";
+  $scope.createQuote = function() {
+    if ($scope.quoteAuthor === '' ||
+        $scope.quoteMessage === '')
+    {
+      return;
+    }
+    var quote = {
+       message: $scope.quoteMessage,
+       author: $scope.quoteAuthor
+     };
+     $scope.quotes.push(quote);
+     $scope.quoteMessage = $scope.quoteAuthor = "";
+  };
+  $scope.deleteQuote = function(quote) {
+    var index = $scope.quotes.indexOf(quote);
+    $scope.quotes.splice(index, 1)
+  }
 
   }]
 );
+directiveMadness.directive('quoteForm', function() {
+  return {
+    templateUrl: 'directives/quoteForm.html',
+    restrict: "E",
+    scope: true
+  };
+})
+directiveMadness.directive('quotesIndex', function() {
+  return {
+    templateUrl: 'directives/quotesIndex.html',
+    restrict: "E",
+    scope: true
+  };
+})
+
 directiveMadness.directive('mainNav', function() {
   return {
     templateUrl: 'directives/mainNav.html',
